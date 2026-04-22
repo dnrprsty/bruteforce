8b8a3751285061414508644819a42d1e918a5174
noer <141932426+dnrprsty@users.noreply.github.com>
2026-04-22 12:09:11 +0700

    Delete distributed-bruteforce directory

--- distributed-bruteforce/utils/generator.py
+++
@@ -1,8 +0,0 @@
-import itertools
-
-def generate_all(charset, length):
-    return [''.join(p) for p in itertools.product(charset, repeat=length)]
-
-def split_list(data, n):
-    k = len(data) // n
-    return [data[i*k:(i+1)*k] for i in range(n-1)] + [data[(n-1)*k:]]
